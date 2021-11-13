472f36dcea3e3184d88a2288280048f7250e9ce6
vluke123 <vluke123@hotmail.co.uk>
2021-11-13 20:38:23 +0000

    annoyed with all formatting

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pythonping import ping
 
 '''
-Commented out whilst trying to figure out how to get a constant output from 'ping':
-
-set_interval = 0.25
-set_count = float(input("For how long (seconds) do you wish to plot your ping for?: ")) / set_interval
+Commented out whilst trying to figure out how to get a constant output
+from 'ping': set_interval = 0.25 set_count = float(input("For how long
+(seconds) do you wish to plot your ping for?: ")) / set_interval
 '''
+
 tagret = "8.8.8.8"
 count = 0
 average_response_list = []
@@ -17,4 +17,5 @@ while True:
     print(f'{average_response_list}')
     average_response_list.append(response.rtt_min_ms)
     average_ms = round(sum(average_response_list) / count, ndigits=2)
-    print(f'{response.rtt_min_ms} ms\nAverage = {average_ms}')
+    print(f'{response.rtt_min_ms} ms\nAverage = {average_ms}\n\
+        Max ms = {max(average_response_list)}')
