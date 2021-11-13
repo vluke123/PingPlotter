e8e8fca1753d395a607c886a697d8156a51c8cc6
vluke123 <vluke123@hotmail.co.uk>
2021-11-13 19:26:14 +0000

    created first loop of ping for rolling avg

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from pythonping import ping
 
-print("test 1")
+'''
+Commented out whilst trying to figure out how to get a constant output from 'ping':
 
-print("testing if I make a change that i can push from VSCode")
+set_interval = 0.25
+set_count = float(input("For how long (seconds) do you wish to plot your ping for?: ")) / set_interval
+'''
+tagret = "8.8.8.8"
+count = 0
+average_response_list = []
 
-Actually testing I can push from VSC and not just commit.
+while True:
+    print(f'count is {count}')
+    response = ping(tagret, count=1, interval=0.25)
+    count += 1
+    print(f'{average_response_list}')
+    average_response_list.append(response.rtt_min_ms)
+    average_ms = round(sum(average_response_list) / count, ndigits=2)
+    print(f'{response.rtt_min_ms} ms\nAverage = {average_ms}')
