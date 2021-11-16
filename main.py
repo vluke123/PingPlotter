3feafbc3e5e9f48fe6c12ffc9d4252d21fb14cdd
vluke123 <vluke123@hotmail.co.uk>
2021-11-16 19:40:54 +0000

    turned into a class

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from pythonping import ping
 
-"""
-Commented out whilst trying to figure out how to get a constant output
-from 'ping': set_interval = 0.25 set_count = float(input("For how long
-(seconds) do you wish to plot your ping for?: ")) / set_interval
-"""
-
 tagret = "8.8.8.8"
-count = 0
 average_response_list = []
 
+
+class PingData:
+    _count = 0
+
+    def __init__(self) -> None:
+        pass
+
+    def _counter(self, count):
+        self._count += 1
+        return self._count
+
+    def ping_loop(self):
+        response = ping(tagret, count=1, interval=0.25)
+        print(f"{average_response_list}")
+        average_response_list.append(response.rtt_min_ms)
+        try:
+            average_ms = round(sum(average_response_list) / self._count, ndigits=2)
+        except ZeroDivisionError:
+            average_ms = round(sum(average_response_list) / 1, ndigits=2)
+        self._counter(self._count)
+        print(
+            f"{response.rtt_min_ms} ms\nAverage = {average_ms}ms\n\
+            Max ms = {max(average_response_list)}ms"
+        )
+
+
+p = PingData()
 while True:
-    print(f"count is {count}")
-    response = ping(tagret, count=1, interval=0.25)
-    count += 1
-    print(f"{average_response_list}")
-    average_response_list.append(response.rtt_min_ms)
-    average_ms = round(sum(average_response_list) / count, ndigits=2)
-    print(
-        f"{response.rtt_min_ms} ms\nAverage = {average_ms}\n\
-        Max ms = {max(average_response_list)}"
-    )
+    p.ping_loop()
+    # print(f"Ping count = {p._count}")
