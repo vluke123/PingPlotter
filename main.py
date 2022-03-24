7b46217c3894c1a1e46d4de1af60d7d1f85ef937
vluke123 <vluke123@hotmail.co.uk>
2022-03-24 11:11:02 +0000

    First working instance of plotting pings

--- main.py
+++ main.py
@@ -24,45 +24,27 @@ class PingData:
         self._count += 1
         return self._count
 
-    def ping_loop(self):
+    def ping_once(self):
         response = ping(tagret, count=1, interval=0.25)
-        # print(f"{average_response_list}")
-        average_response_list.append(response.rtt_min_ms)
-        try:
-            average_ms = round(sum(average_response_list) / self._count, ndigits=2)
-        except ZeroDivisionError:
-            average_ms = round(sum(average_response_list) / 1, ndigits=2)
         self._counter(self._count)
-        # print(f"{response.rtt_min_ms} ms\naverage ms = {average_ms}ms")
-        # print(f"max ms = {max(average_response_list)}ms")
-        # print(f"min ms = {min(average_response_list)}ms")
+        print(response)
+        return response.rtt_max_ms
+
 
 
 p = PingData()
-while True:
-    p.ping_loop()
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    ax1.set_title('Ping returns (ms)')
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax1.set_title('Ping returns (ms)')
 
-    def animate(i):
-        dt = datetime.now()
-        time_vals.append(dt)
-        ax1.plot(time_vals, average_response_list, 'green')
-
-    ani = animation.FuncAnimation(fig, animate, interval=250)
-
-    plt.tight_layout()
-    plt.show()
-
-'''
 def animate(i):
     dt = datetime.now()
+    ping_return = p.ping_once()
+    average_response_list.append(ping_return)
     time_vals.append(dt)
-    r1 = random.randint(1,100)
-    average_response_list.append(r1)
     ax1.plot(time_vals, average_response_list, 'green')
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=250)
+
+plt.tight_layout()
 plt.show()
-'''
